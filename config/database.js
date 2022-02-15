05188ff2626fb3fef5baf4d6be840bfa7470ad6e
Amin Sehati <Aminsehati70@gmail.com>
2022-02-15 09:41:28 +0330

    add requst for tacks

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+// mongodb+srv://root:PJkIxEoQEJBENLnf@mrtehran.xc53v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 module.exports = () => {
     mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD }@mrtehran.xc53v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
