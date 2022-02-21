9d50aecffdf48a062dbad62d79219aed525c5bd3
Amin Sehati <Aminsehati70@gmail.com>
2022-02-21 19:10:52 +0330

    updated dev

---
+++ types/auth/index.js
@@ -0,0 +1,11 @@
+const {
+    gql
+} = require("apollo-server-express")
+module.exports = gql `
+type Mutation {
+    registerUser:User,
+}
+type User {
+    firstName:String,
+}
+`
--- types/index.js
+++ types/index.js
@@ -1,10 +1,12 @@
 const actors = require('./actors');
 const upload = require('./upload');
 const playList = require('./playList')
-const track = require('./track')
+const track = require('./track');
+const auth = require('./auth')
 module.exports = [
     actors,
     upload,
     playList,
-    track
+    track,
+    auth
 ]
